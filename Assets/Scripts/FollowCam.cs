3a37f6a197d8f267cda6777d4b626b5a775e0ddc
Jean Woo Ha <jeanha124@gmail.com>
2016-06-14 19:43:13 -0400

    Final Commit: Connect the games
    Code the game and put all the levels together with the GUIText

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -2,52 +2,52 @@
 using System.Collections;
 
 public class FollowCam : MonoBehaviour {
-	static public FollowCam S; //a FollowCam Singleton
+	static public FollowCam S; 
 
-	//fields set in the Unity Inspector pane
+
 	public float easing = 0.05f;
 	public Vector2 minXY;
 	public bool _________________________;
 
-	//fields set dynamically
-	public GameObject poi; //The point of interest
-	public float camZ; //The desired Z pos of the camera
+
+	public GameObject poi; 
+	public float camZ;
 
 	void Awake(){
 		S = this;
 		camZ = this.transform.position.z;
 	}
 
-	void fixedUpdate(){
+	void FixedUpdate(){
 		Vector3 destination;
-		//If therre is no poi, rturn to P: [0,0,0]
+	
 		if (poi == null) {
 			destination = Vector3.zero;
 		} else {
-			//Get the position of the poi
+		
 			destination = poi.transform.position;
-			//If poi is a Projectile, check to see if it's at rest
+		
 			if (poi.tag == "Projectile"){
-				//if it is sleeping (that is, not moving)
+
 				if (poi.rigidbody.IsSleeping()) {
-					//return to default view
+				
 					poi = null;
-					//in the next update
+
 					return;
 				}
 			}
 		}
-		//Limit the X & Y to minimum values
+
 		destination.x = Mathf.Max (minXY.x, destination.x);
 		destination.y = Mathf.Max (minXY.y, destination.y);
 
-		//Interpolate from the current Camera position toward destination
+	
 		destination = Vector3.Lerp(transform.position, destination, easing);
-		//Retain a destination.z of camZ
+	
 		destination.z = camZ;
-		//Set the camera to the destination
+	
 		transform.position = destination;
-		// Set the orthographicSize of the Camera to keep Ground in view
+
 		this.camera.orthographicSize = destination.y + 10;
 	}
 }
